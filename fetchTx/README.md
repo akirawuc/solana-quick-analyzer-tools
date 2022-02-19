d43315edcc1c787b8af46040d275ce27ea42d051
akirawu <stu970029@gmail.com>
2022-02-19 17:31:36 +0800

    feat: add in the tx quick fetching and parsing tool

---
+++ fetchTx/README.md
@@ -0,0 +1,13 @@
+# HOW TO USE IT
+
+## Usage
+
+A tool for quickly fetching the content of a tx on Solana, and print the parsed results on screen.
+
+## Steps
+
+1. Install Docker
+
+2. Run `docker run akichill/soltx -s replace_here_with_the_signature`, and replace the signature without quotation mark.
+
+
---
+++ fetchTx/fetchTx.py
@@ -0,0 +1,29 @@
+import requests as r
+import json
+import argparse
+parser = argparse.ArgumentParser(description = 'help getting the tx')
+parser.add_argument('-s', '--sig', type=str, help='the signature of the tx')
+args = parser.parse_args()
+def getTx(sig):
+    url = 'http://api.mainnet-beta.solana.com'
+    content = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "getConfirmedTransaction",
+        "params": [
+          sig,
+          "json"
+        ]
+      }
+    return r.post(url, json =content)
+
+def replacingInvalid(string:str)-> str:
+    string = string.replace('null', '"null"')
+    string = string.replace('false', 'False')
+    string = string.replace('true', 'True')
+    return string
+
+if __name__ == '__main__':
+    print(f'Checking the content of {args.sig}:')  
+    thisTx = eval(replacingInvalid(getTx(args.sig).text))
+    print(json.dumps(thisTx, indent = 3))
